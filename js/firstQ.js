b5c627220e4d7976f2c367d049d5d1d86a412527
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-07 15:34:35 -0500

    kentucky3

--- js/firstQ.js
+++ js/firstQ.js
@@ -5,5 +5,5 @@ var runGame = function()
   var button1 = document.getElementById("startButton");
   button1.remove();
   var kentucky = "How many states border Kentucky";
-  openingText.after(kentucky);
+  console.log(kentucky);
 }
