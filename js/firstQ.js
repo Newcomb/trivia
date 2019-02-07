06f1353888ac3fd04c41233388364555487da6c2
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-07 15:44:31 -0500

    first button

--- js/firstQ.js
+++ js/firstQ.js
@@ -2,8 +2,11 @@ var runGame = function()
 {
   var openingText = document.getElementById("open");
   openingText.innerText = "First Question";
-  var button1 = document.getElementById("startButton");
-  button1.remove();
-  var kentucky = "How many states border Kentucky";
+  var buttonStart = document.getElementById("startButton");
+  buttonStart.remove();
+  var kentucky = "How many states border Kentucky?";
   console.log(kentucky);
+  var button1 = document.createElement("button");
+  button1.setAttribute(text = "")
+
 }
