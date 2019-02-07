5100b1d8dec2f2ac832ef1fd54a03160b4e0ded7
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-07 15:23:17 -0500

    c

--- js/firstQ.js
+++ js/firstQ.js
@@ -1,7 +1,7 @@
 var runGame = function()
 {
-  var openingText = document.getElementById("open")
-  openingText.innerText = "First Question"
-  var button1 = document.getElementById("startButton")
-  button1.remove()
+  var openingText = document.getElementById("open");
+  openingText.innerText = "First Question";
+  var button1 = document.getElementById("startButton");
+  button1.remove();
 }
