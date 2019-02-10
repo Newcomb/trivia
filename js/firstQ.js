953df3dfa95f455a1de47605016a8f4e53716456
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-10 16:03:06 -0500

    question

--- js/firstQ.js
+++ js/firstQ.js
@@ -5,7 +5,7 @@ var runGame = function()
   var buttonStart = document.getElementById("startButton");
   buttonStart.remove();
   var kentucky = "How many states border Kentucky?";
-  console.log(kentucky);
+  document.log(kentucky);
   var button1 = document.createElement("button");
   button1.setAttribute(text = "")
 
