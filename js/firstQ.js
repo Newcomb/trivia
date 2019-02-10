01410b993d4b3e43ef22d55477e9b4cd1aa61c01
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-10 16:26:15 -0500

    changes

--- js/firstQ.js
+++ js/firstQ.js
@@ -1,14 +1,11 @@
 var runGame = function()
 {
   var openingText = document.getElementById("open");
-  openingText.innerText = "First Question";
+  openingText.innerText = "How many states border Kentucky?";
   var buttonStart = document.getElementById("startButton");
   buttonStart.remove();
-  var kentucky = document.getElementById("questionSpot");
-  kentucky.innerText = "How many states border Kentucky?";
-  var button1 = document.createElement("button");
-  var buttonAnswer = document.createTextNode(text = "7")
-  button1.appendChild(buttonAnswer)
-  
+
+
+
 
 }
