92c7deaeac78ffa87d21f848f08bfc830ae154b5
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-10 16:38:06 -0500

    buttons

--- js/firstQ.js
+++ js/firstQ.js
@@ -5,8 +5,11 @@ var runGame = function()
   var buttonStart = document.getElementById("startButton");
   buttonStart.remove();
   var button1 = document.getElementById("answer1")
-  button1.innerHTML = "7"
-
+  button1.innerHTML = "5"
+  var button2 = document.getElementById("answer2")
+  button2.innerHTML = "6"
+  var button3 = document.getElementById("answer3")
+  button3.innerHTML = "7"
 
 
 
