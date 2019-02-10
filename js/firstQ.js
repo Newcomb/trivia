0e3f03e1fbecadfb1a02954c8b803bbe5258cf36
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-10 16:50:03 -0500

    wrong

--- js/firstQ.js
+++ js/firstQ.js
@@ -4,13 +4,18 @@ var runGame = function()
   openingText.innerText = "How many states border Kentucky?";
   var buttonStart = document.getElementById("startButton");
   buttonStart.remove();
-  var button1 = document.getElementById("answer1")
-  button1.innerHTML = "5"
-  var button2 = document.getElementById("answer2")
-  button2.innerHTML = "6"
-  var button3 = document.getElementById("answer3")
-  button3.innerHTML = "7"
+  var button1 = document.getElementById("answer1");
+  button1.setAttribute(onclick,wrong());
+  button1.innerHTML = "5";
+  var button2 = document.getElementById("answer2");
+  button2.setAttribute(onclick,wrong());
+  button2.innerHTML = "6";
+  var button3 = document.getElementById("answer3");
+  button1.setAttribute(onclick,right());
+  button3.innerHTML = "7";
+}
 
-
-
+var wrong=function()
+{var openingText=document.getElementById("open");
+openingText.innerText="Spinach is higher in what element?";
 }
